e683b7684741dbdcab07d8c684c6b227cc1e81c0
Renu Sree <renusree0704@gmail.com>
2024-11-30 14:04:45 +0530

    Delete Debugging directory

--- Debugging/ES6/create-an-export-fallback-with-export-default.js
+++
@@ -1,6 +0,0 @@
-
-function subtract(x, y) {
-  return x - y;
-}
-
-export default function subtract(x,y) {return x - y;}
--- Debugging/ES6/reuse-javascript-code-using-import.js
+++
@@ -1,4 +0,0 @@
-import { uppercaseString, lowercaseString } from './string_functions.js';
-
-uppercaseString("hello");
-lowercaseString("WORLD!");
